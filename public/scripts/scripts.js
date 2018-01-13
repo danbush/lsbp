224a4967f29d45ac2b0a71982b7b1305c21b9e32
danbush <dbush89@gmail.com>
2018-01-12 20:42:12 -0800

    added a basic chat system just to test websockets.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -2,6 +2,34 @@
 // Make connection
 var socket = io.connect();
 
+// Query DOM
+var message = document.getElementById('chat-message');
+var handle = document.getElementById('chat-handle');
+var btn = document.getElementById('chat-send');
+var output = document.getElementById('chat-output');
+var feedback = document.getElementById('chat-feedback');
+
+// Emit events
+btn.addEventListener('click', function() {
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+});
+message.addEventListener('keypress', function() {
+  socket.emit('typing', handle.value);
+});
+
+// Listen for events
+socket.on('chat', function(data) {
+  feedback.innerHTML = "";
+  output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data) {
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+
 //interact js
 // target elements with the "draggable" class
   interact('.draggable')
